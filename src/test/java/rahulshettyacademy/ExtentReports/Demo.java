2c1902912f669ab187997f94172a8d526de8bd3b
negiMayank <mayanknegi0017@gmail.com>
2024-06-07 16:31:17 +0530

    final commit

---
+++ src/test/java/rahulshettyacademy/ExtentReports/Demo.java
@@ -0,0 +1,12 @@
+package rahulshettyacademy.ExtentReports;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void demoMethod() {
+		
+		System.out.println("Demo Method");
+	}
+}
